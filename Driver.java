903f0b4a3f29a4053b508159e0acf49cadcd71f2
Randyll Bearer <rlb97@pitt.edu>
2017-10-10 03:22:14 -0400

    Initial Commit
    First Commit, Still need to clean up some comments. Program meets all
    requirements and all unit tests pass.

---
+++ Driver.java
@@ -0,0 +1,34 @@
+
+public class Driver{
+	String name;
+	Location currentLocation;
+	
+	public boolean setName(String desiredName){
+		if(desiredName != null){
+			name = desiredName;
+			return true;
+		}else{
+			System.out.println("ERROR: Cannot make a driver's name NULL");
+			return false;
+		}
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public boolean setCurrentLocation(Location newCurrentLocation){
+		if(newCurrentLocation != null){
+			currentLocation = newCurrentLocation;
+			return true;
+		}else{
+			System.out.println("ERROR: Cannot place a driver at a NULL location");
+			return false;
+		}
+	}
+	
+	public Location getCurrentLocation(){
+		return currentLocation;
+	}
+	
+}
